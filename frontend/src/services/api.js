9b754d229b91795100c001e47068b48ded703968
Sahib Singh <sahibzworld@gmail.com>
2026-07-02 19:53:45 +0530

    Updated Frontend for render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mediconnect-86xp.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
